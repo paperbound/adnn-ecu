677a07a8c1833eb2df9b183a236fc652a80b263f
Deepak <deepakv.3295@gmail.com>
2023-02-22 17:29:30 +0100

    tf.lite convert model script

---
+++ hdrnn/python-tf/convert_model.py
@@ -0,0 +1,8 @@
+
+import tensorflow as tf
+
+model = tf.keras.models.load_model('trained_model')
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+tf.lite.experimental.Analyzer.analyze(model_content = tflite_model)
