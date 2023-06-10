4be5355d73d51acbe8ef61954cb48f411480401c
Prasanth Thomas Shaji <prasanthshaji@icloud.com>
2023-06-11 01:44:07 +0200

    Introduces cli-args to cpp-libtorch

--- hdrnn/cpp-libtorch/README.md
+++ hdrnn/cpp-libtorch/README.md
@@ -2,20 +2,94 @@
 
 Derived from example application provided by `torch`
 
-Building this project requires the use of libtorch library
+Building this project requires the use of libtorch library and the MNIST dataset
+
+See https://github.com/pytorch/pytorch#from-source for instructions on building PyTorch
 
 ## Compiling
 
-Create a build directory, run cmake with path to libtorch
+Create a build directory (mkdir -p build), run `cmake` with path to libtorch
 
 ```
-$ cd mnist
-$ mkdir build
-$ cd build
-$ cmake -DCMAKE_PREFIX_PATH=/path/to/libtorch ..
-$ make
+cmake -DCMAKE_PREFIX_PATH=/path/to/libtorch ..
+```
+
+```
+cmake --build .
 ```
 
 ## Usage comments
 
 Run the binary normally, `./mnist`
+
+### Inference
+
+```
+./mnist infer <IMAGE> --net <NETWORK>
+```
+
+You can create images using the methods specified in this README.md
+
+It is assumed that the image will be a kind of PGM (2) format
+
+The paths to the weights and biases files must contain HDR-NN weights and biases created by the `python-numpy`, `c-math.h`, or `cpp-eigen` programs
+
+### Training
+
+```
+./mnist train --net <NETWORK>
+```
+
+Default value assumed for net as `c-math.nn`
+
+#### Silent invocation
+
+Use silent invocation using the `--quiet` or `-q` flag
+
+When used with the `train` command, the final weights and biases files won't be generated
+
+```
+./mnist train -q
+```
+
+### Parameters for the Learning Algorithm
+
+#### Epochs
+
+> `--epochs` `-e` N
+
+The number of epochs to train the network
+
+```
+./mnist train -e 21
+```
+
+#### Network Shape
+
+> `--shape` `-s` N,+
+
+The size and shape of the HDR-NN network as comma seperated numbers
+
+```
+./mnist train --shape 16,16
+```
+
+#### Learning Rate
+
+> `--learning_rate` `-lr` F
+
+The learning rate for the Stochastic Gradient Descent as a floating point value
+
+```
+./mnist train -lr 0.3
+```
+
+#### Batch Size
+
+> `--batch_size` `-bs` N
+
+The batch size while performing mSGD as an integer
+
+```
+./mnist train -bs 64
+```
--- hdrnn/cpp-libtorch/mnist.cpp
+++ hdrnn/cpp-libtorch/mnist.cpp
@@ -1,85 +1,215 @@
 #include <torch/torch.h>
+#include <gflags/gflags.h>
+
+DEFINE_bool(quiet, false, "Path to the training data for HDR-NN");
+DEFINE_string(shape, "32", "Shape of the HDR-NN");
+DEFINE_uint32(epochs, 30, "The number of epochs to train the network");
+DEFINE_uint32(batch_size, 10, "The batch size while performing mSGD");
+DEFINE_double(learning_rate, 3.0, "The learning rate for SGD");
+DEFINE_string(net, "net.pt", "File path for .pt file");
+DEFINE_string(image, "1.pgm", "PGM image file path");
+DEFINE_string(mnist, "./data", "MNIST dataset");
+
+const unsigned int IMAGE_SIZE = 784;
+const unsigned int DIGITS = 10;
+
+const char *help = "\n"
+	"\t<command> [<args>]\n\nCommands:\n"
+	"\t\033[1minfer\033[22m [--image IMAGE_PATH] [--net net.pt]\n"
+	"\t\033[1mtrain\033[22m [--shape 30] [--epochs 30]\n"
+	"\t\033\t\033 [--quiet] [--batch_size=10]\n"
+	"\t\033\t\033 [--learning_rate 3] [--net net.pt]\n";
+
+enum command { INFER, TRAIN, NONE };
+
+/* Parse command */
+enum command parseCommand(char *c)
+{
+	if (c != NULL && strcmp("train", c) == 0)
+	{
+		return TRAIN;
+	}
+	else if (c != NULL && strcmp("infer", c) == 0)
+	{
+		return INFER;
+	}
+
+	return NONE;
+}
+
+std::vector<unsigned int> parseShape(std::string shape_str)
+{
+	std::size_t n_size{}, index{};
+	std::vector<unsigned int> shape;
+
+	while (index < shape_str.length())
+	{
+		try
+		{
+			const unsigned int i{
+				static_cast<unsigned int>(
+					std::stoi(shape_str.substr(index), &n_size))};
+			shape.push_back(i);
+			index = index + n_size + 1;
+		}
+		catch (std::invalid_argument const &ex)
+		{
+			std::cerr << "std::invalid_argument::what(): " << ex.what() << help;
+			std::exit(EXIT_FAILURE);
+		}
+		catch (std::out_of_range const &ex)
+		{
+			std::cout << "std::out_of_range::what(): " << ex.what() << help;
+			std::exit(EXIT_FAILURE);
+		}
+	}
+
+	return shape;
+}
 
 // Define a new Module.
-struct Net : torch::nn::Module {
-  Net() {
-    // Construct and register two Linear submodules.
-    fc1 = register_module("fc1", torch::nn::Linear(784, 30));
-    fc2 = register_module("fc2", torch::nn::Linear(30, 10));
-  }
+struct Net : torch::nn::Module
+{
+	Net(std::vector<unsigned int> shape)
+	{
+		// size of image in row vector form
+		unsigned int previous_dim = IMAGE_SIZE;
+		unsigned int index = 1;
+		torch::nn::Linear fc{nullptr};
 
-  // Implement the Net's algorithm.
-  torch::Tensor forward(torch::Tensor x) {
-    // Use one of many tensor manipulation functions.
-    x = torch::sigmoid(fc1->forward(x.reshape({x.size(0), 784})));
-    x = torch::sigmoid(fc2->forward(x));
-    return x;
-  }
+		// iterate over the dimensions required in the hidden layer
+		for (auto c_dim : shape)
+		{
+			// Construct and register Linear submodules.
+			fc = register_module(
+				"fc" + std::to_string(index),
+				torch::nn::Linear(previous_dim, c_dim));
+			previous_dim = c_dim;
+			index++;
+			fc_list.push_back(fc);
+		}
 
-  // Use one of many "standard library" modules.
-  torch::nn::Linear fc1{nullptr}, fc2{nullptr};
+		// finally, add the last layer
+		fc = register_module(
+			"fc" + std::to_string(index),
+			torch::nn::Linear(previous_dim, DIGITS));
+		fc_list.push_back(fc);
+	}
+
+	// Implement the Net's algorithm.
+	torch::Tensor forward(torch::Tensor x)
+	{
+		// Use one of many tensor manipulation functions.
+		x = x.reshape({x.size(0), 784});
+		for (auto fc : fc_list)
+			x = torch::sigmoid(fc->forward(x));
+
+		return x;
+	}
+
+	// Use one of many "standard library" modules.
+	std::vector<torch::nn::Linear> fc_list{};
 };
 
-int main() {
-  // Create a new Net.
-  auto net = std::make_shared<Net>();
+int main(int argc, char *argv[])
+{
+	gflags::SetUsageMessage(help);
+	gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-  torch::set_num_threads(1);
-  torch::set_num_interop_threads(1);
+	torch::set_num_threads(1);
+	torch::set_num_interop_threads(1);
 
-  // Create a multi-threaded data loader for the MNIST dataset.
-  auto data_loader = torch::data::make_data_loader(
-      torch::data::datasets::MNIST("./data").map(
-          torch::data::transforms::Stack<>()),
-      /*batch_size=*/10);
+	// TODO: allow after porting mnist.h
+	// std::ios_base::sync_with_stdio(false);
 
-  auto test_data_loader = torch::data::make_data_loader(
-	torch::data::datasets::MNIST("./data",
-	     torch::data::datasets::MNIST::Mode::kTest).map(
-		    torch::data::transforms::Stack<>()));
+	std::vector<unsigned int> shape = parseShape(FLAGS_shape);
 
-  // Instantiate an SGD optimization algorithm to update our Net's parameters.
-  torch::optim::SGD optimizer(net->parameters(), /*lr=*/3.0);
+	switch (parseCommand(argv[1]))
+	{
+		case INFER:
+		{
+			// Create a new Net.
+			auto net = std::make_shared<Net>(shape);
+			torch::load(net, FLAGS_net);
 
-  for (size_t epoch = 1; epoch <= 30; ++epoch) {
-    size_t batch_index = 0;
-    // Iterate the data loader to yield batches from the dataset.
-    for (auto& batch : *data_loader) {
-      // Reset gradients.
-      optimizer.zero_grad();
-      // Execute the model on the input data.
-      torch::Tensor prediction = net->forward(batch.data);
-      torch::Tensor target = torch::one_hot(batch.target, 10).to(torch::kFloat32);
-      // Compute a loss value to judge the prediction of our model.
-      torch::Tensor loss = torch::mse_loss(
-					   prediction,
-					   target
-					   );
-      // Compute gradients of the loss w.r.t. the parameters of our model.
-      loss.backward();
-      // Update the parameters based on the calculated gradients.
-      optimizer.step();
-    }
+			break;
+		}
+		case TRAIN:
+		{
+			// Create a new Net.
+			auto net = std::make_shared<Net>(shape);
 
-    // Test, output loss, and checkpoint every epoch
-    double test_loss = 0;
-    long correct = 0;
-    for (const auto& batch : *test_data_loader) {
-	    torch::Tensor prediction = net->forward(batch.data);
-	    torch::Tensor target = torch::one_hot(batch.target, 10)
-		    .to(torch::kFloat32);
-	    torch::Tensor loss = torch::mse_loss(
-						 prediction,
-						 target
-						 );
-	    test_loss += loss.item<float>();
-	    correct += (target.argmax(1).item<int>() - prediction.argmax(1).item<int>() == 0);
-    }
+			// Create a multi-threaded data loader for the MNIST dataset.
+			auto data_loader = torch::data::make_data_loader(
+				torch::data::datasets::MNIST(FLAGS_mnist)
+				.map(torch::data::transforms::Stack<>()),
+					/*batch_size=*/FLAGS_batch_size);
 
-    // Output loss and checkpoint every epoch
-    std::cout << "Epoch: " << epoch << " | Loss: " << test_loss
-	      << "| Correct: " << correct << std::endl;
-    // Serialize your model periodically as a checkpoint.
-    torch::save(net, "net.pt");
-  }
+			auto test_data_loader = torch::data::make_data_loader(
+				torch::data::datasets::MNIST(FLAGS_mnist,
+					torch::data::datasets::MNIST::Mode::kTest)
+						.map(torch::data::transforms::Stack<>()));
+
+			// Instantiate an SGD optimization algorithm to
+			// update our Net's parameters.
+			torch::optim::SGD optimizer(net->parameters(),
+										/*lr=*/FLAGS_learning_rate);
+
+			for (size_t epoch = 1; epoch <= FLAGS_epochs; ++epoch)
+			{
+				size_t batch_index = 0;
+				// Iterate the data loader to yield batches from the dataset.
+				for (auto &batch : *data_loader)
+				{
+					// Reset gradients.
+					optimizer.zero_grad();
+					// Execute the model on the input data.
+					torch::Tensor prediction = net->forward(batch.data);
+					torch::Tensor target = torch::one_hot(
+											batch.target, 10)
+											.to(torch::kFloat32);
+					// Compute a loss value
+					// to judge the prediction of our model.
+					torch::Tensor loss = torch::mse_loss(
+						prediction,
+						target);
+					// Compute gradients of the loss w.r.t.
+					// the parameters of our model.
+					loss.backward();
+					// Update the parameters based on the calculated gradients.
+					optimizer.step();
+				}
+
+				// Test, output loss, and checkpoint every epoch
+				double test_loss = 0;
+				long correct = 0;
+				for (const auto &batch : *test_data_loader)
+				{
+					torch::Tensor prediction = net->forward(batch.data);
+					torch::Tensor target = torch::one_hot(batch.target, 10)
+											.to(torch::kFloat32);
+					torch::Tensor loss = torch::mse_loss(
+						prediction,
+						target);
+					test_loss += loss.item<float>();
+					correct += (
+						target.argmax(1).item<int>() -
+						prediction.argmax(1).item<int>() == 0);
+				}
+
+				if (!FLAGS_quiet)
+					std::cout << "Epoch: " << epoch << " | Loss: " << test_loss
+							<< "| Correct: " << correct << std::endl;
+			}
+
+			if (!FLAGS_quiet)
+				torch::save(net, "net.pt");
+
+			break;
+		}
+		case NONE:
+		{
+			std::cout << "nothing to do" << help;
+		}
+	}
 }
